9afd00c822b972aff7d5708896a17ac7133e1969
Frank Hamand <frankhamand@gmail.com>
2017-04-28 13:33:10 +0100

    Add tests

--- spec/pascal_spec.cr
+++ spec/pascal_spec.cr
@@ -1,9 +1,21 @@
 require "./spec_helper"
 
 describe Pascal do
-  # TODO: Write tests
+  it "next_row" do
+    Pascal.next_row([1]).should eq([1, 1])
+    Pascal.next_row([1, 1]).should eq([1, 2, 1])
+    Pascal.next_row([1, 2, 1]).should eq([1, 3, 3, 1])
+  end
 
-  it "works" do
-    false.should eq(true)
+  it "pascal" do
+    Pascal.pascal(0).should eq([] of Array(Int32))
+    Pascal.pascal(1).should eq([[1]])
+    Pascal.pascal(2).should eq([[1], [1, 1]])
+    Pascal.pascal(3).should eq([[1], [1, 1], [1, 2, 1]])
+    Pascal.pascal(4).should eq([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]])
+  end
+
+  it "pascal large" do
+    Pascal.pascal(1000)
   end
 end
